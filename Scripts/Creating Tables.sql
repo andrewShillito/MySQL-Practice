e10bed74deefcb6acb913562ded7ce6b66142224
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-27 14:06:39 -0500

    feat: begin exercises in MYSQL

---
+++ Scripts/Creating Tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cats (
+    name VARCHAR(100),
+    breed VARCHAR(100),
+    age INT
+);
+
+CREATE TABLE people (
+	first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    age TINYINT,
+    birth_place VARCHAR(50)
+);
