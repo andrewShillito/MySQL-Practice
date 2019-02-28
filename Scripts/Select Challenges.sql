0129d0afc9cc59d9f00760e53e938921faf244c0
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-02-28 15:12:14 -0500

    practice: selection practice

---
+++ Scripts/Select Challenges.sql
@@ -0,0 +1,5 @@
+SELECT id AS cat_id FROM cats;
+SELECT name, breed FROM cats;
+SELECT name, age FROM cats WHERE breed="Tabby";
+SELECT id AS cat_id, age FROM cats WHERE id=age;
+SELECT * FROM cats WHERE id=age;
